ba54bf48e8c6f066b831d2a8e9d4277adae1003b
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2024-08-15 19:17:31 +0530

    time&date

--- src/app/calendar/calendar/calendar.component.ts
+++ src/app/calendar/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -15,10 +16,38 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatNativeDateModule,
     MatInputModule,
     MatToolbarModule,
-    MatButtonModule,],
+    MatButtonModule,FormsModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
   selectedDate: Date | null = null;
+  // selectedTime: string;
+
+  // constructor() {
+  //   this.selectedTime = '12:00'; // Default time
+  // }
+
+
+
+  // hours: number[] = Array.from({ length: 24 }, (_, i) => i);
+  // minutes: number[] = Array.from({ length: 60 }, (_, i) => i);
+  // selectedHour: number = 12;
+  // selectedMinute: number = 0;
+  // timeChange: any;
+  // ngOnChanges() {
+  //   this.emitTime();
+  // }
+  // emitTime() {
+  //   const time = `${this.pad(this.selectedHour)}:${this.pad(this.selectedMinute)}`;
+  //   this.timeChange.emit(time);
+  // }
+  // pad(value: number): string {
+  //   return value < 10 ? '0' + value : value.toString();
+  // }
+
+  onTimeChange(event: any): void {
+    const selectedTime = event.value;
+    console.log('Selected Time:', selectedTime);
+  }
 }
